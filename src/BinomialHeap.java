81e9381a14d14523d7a0417647665f1c76e8fd81
xqgex <xqgex.n@gmail.com>
2015-12-22 23:15:26 +0200

    Add help documents and create the basic ADT functions.

--- src/BinomialHeap.java
+++ src/BinomialHeap.java
@@ -6,6 +6,104 @@
  */
 public class BinomialHeap
 {
+	/***************************************************
+	 ****			Variables						****
+	 ***************************************************/
+	public int numberOfLinks;
+	/***************************************************
+	 ****			ADT functions					****
+	 ***************************************************/
+	private Node minimum(BinomialHeap H) {
+		y = NIL;
+		x = head[H];
+		min = infinity;
+		while (x != NIL) {
+			if (key[x] < min) {
+				min = key[x];
+				y = x;
+			}
+			x = sibling[x];
+		}
+		return y;
+	}
+	private void link(Node y, Node z) {
+		p[y] = z;
+		sibling[y] = child[z];
+		child[z] = y;
+		degree[z] += 1;
+	}
+	private BinomialHeap union(BinomialHeap H1, BinomialHeap H2) {
+		BinomialHeap H = new BinomialHeap();
+		head[H] = merge(H1,H2);
+		// free the objects H1 and H2 but not the lists they point to
+		if (head[H] == NIL) {
+			return H;
+		}
+		prev_x = NIL;
+		x = head[H];
+		next_x = sibling[x];
+		while (next_x != NIL) {
+			if ( (degree[x] != degree[next_x])||( (sibling[next_x] != NIL)&&(degree[sibling[next_x]] == degree[x]) ) ) { // Case 1+2
+				prev_x = x;
+				x = next_x;
+			} else { // Case 3+4
+				if (key[x] <= key[next_x]) { // Case 3
+					link(next_x,x);
+				} else { // Case 4
+					if (prev_x == NIL) {
+						head[H] = next_x;
+					} else {
+						sibling[prev_x] = next_x;
+					}
+					link(x,next_x);
+					x = next_x;
+				}
+			}
+			next_x = sibling[x];
+		}
+		return H;
+	}
+	private void insert(BinomialHeap H, int x) {
+		BinomialHeap H1 = new BinomialHeap();
+		p[x] = NIL;
+		child[x] = NIL;
+		sibling[x] = NIL;
+		degree[x] = 0;
+		head[H1] = x;
+		H = union(H,H1)
+	}
+	private int extractMin(BinomialHeap H) {
+		// find the root x with the minimum key in the root list of H , and remove x from the root list of H
+		BinomialHeap H1 = new BinomialHeap();
+		// reverse the order of the linked list of x’s children, setting the p field of each child to NIL , and set head[H ′ ] to point to the head of the resulting list
+		H = union(H,H1);
+		return x;
+	}
+	private void decreaseKey(BinomialHeap H, int x, k) {
+		if (k > key[x]) {
+			System.err.println("New key is greater than current key");
+		} else {
+			key[x] = k;
+			y = x;
+			z = p[y];
+			while ( (z != NIL)&&(key[y] < key[z]) ) {
+				tmpKey = key[z];
+				key[z] = key[y];
+				key[y] = tmpKey;
+				if (If y and z have satellite fields, exchange them, too.) { // If y and z have satellite fields, exchange them, too.
+					y = z;
+					z = p[y];
+				}
+			}
+		}
+	}
+	private void delete(BinomialHeap H, int x) {
+		decreaseKey(H, x, -infinity);
+		extractMin( H);
+	}
+	/***************************************************
+	 ****			Public functions				****
+	 ***************************************************/
 	/**
 	* public boolean empty()
 	*
