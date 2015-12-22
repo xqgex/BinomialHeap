e2c4391e84af9c0e7e1baf40ff8da1a170c17dc5
XqG3X <xqg3x@XqG3X>
2015-12-22 22:18:59 +0200

    first commit

---
+++ src/BinomialHeap.java
@@ -0,0 +1,124 @@
+/**
+ * BinomialHeap
+ *
+ * An implementation of binomial heap over non-negative integers.
+ * Based on exercise from previous semester.
+ */
+public class BinomialHeap
+{
+	/**
+	* public boolean empty()
+	*
+	* precondition: none
+	* 
+	* The method returns true if and only if the heap
+	* is empty.
+	*	
+	*/
+	public boolean empty()
+	{
+		return false; // should be replaced by student code
+	}
+	/**
+	* public void insert(int value)
+	*
+	* Insert value into the heap 
+	*
+	*/
+	public void insert(int value) 
+	{	
+		return; // should be replaced by student code
+	}
+	/**
+	* public void deleteMin()
+	*
+	* Delete the minimum value
+	*
+	*/
+	public void deleteMin()
+	{
+		return; // should be replaced by student code
+		
+	}
+	/**
+	* public int findMin()
+	*
+	* Return the minimum value
+	*
+	*/
+	public int findMin()
+	{
+		return 42;// should be replaced by student code
+	} 
+	/**
+	* public void meld (BinomialHeap heap2)
+	*
+	* Meld the heap with heap2
+	*
+	*/
+	public void meld (BinomialHeap heap2)
+	{
+		return; // should be replaced by student code			
+	}
+	/**
+	* public int size()
+	*
+	* Return the number of elements in the heap
+	*	
+	*/
+	public int size()
+	{
+		return 42; // should be replaced by student code
+	}
+	/**
+	* public int minTreeRank()
+	*
+	* Return the minimum rank of a tree in the heap.
+	* 
+	*/
+	public int minTreeRank()
+	{
+		return 0; //	to be replaced by student code
+	}
+	/**
+	* public boolean[] binaryRep()
+	*
+	* Return an array containing the binary representation of the heap.
+	* 
+	*/
+	public boolean[] binaryRep()
+	{
+		boolean[] arr = new boolean[42];
+		return arr; //	to be replaced by student code
+	}
+	/**
+	* public void arrayToHeap()
+	*
+	* Insert the array to the heap. Delete previous elemnts in the heap.
+	* 
+	*/
+	public void arrayToHeap(int[] array)
+	{
+		return; //	to be replaced by student code
+	}
+	/**
+	* public boolean isValid()
+	*
+	* Returns true if and only if the heap is valid.
+	*	
+	*/
+	public boolean isValid() 
+	{
+		return false; // should be replaced by student code
+	}
+	/**
+	* public class HeapNode
+	* 
+	* If you wish to implement classes other than BinomialHeap
+	* (for example HeapNode), do it in this file, not in 
+	* another file 
+	*  
+	*/
+	public class HeapNode{
+	}
+}
